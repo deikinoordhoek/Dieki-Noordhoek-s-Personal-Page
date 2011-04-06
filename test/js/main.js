ccaf938b16d44c704980a99486f8d699bd34027d
unknown <Dieki@.(none)>
2011-04-06 09:16:44 -0500

    Test page

--- test/js/main.js
+++ test/js/main.js
@@ -1,5 +1,11 @@
+
+
 function drawcanvas(){
+
 	can = document.getElementById("canvas-1")
+	if (typeof FlashCanvas != "undefined") {
+	    FlashCanvas.initElement(can);
+	}
 	con = can.getContext("2d")
 	con.fillStyle = "#ffffff"
 	con.fillRect(50,25,150,100)
